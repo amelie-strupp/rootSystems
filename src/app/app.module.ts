b6a0b819c1c0d546e7fa54989b564583d970d512
Amelie Strupp <amelie.strupp0@gmail.com>
2022-11-15 13:56:08 +0100

    Fixed wrong root base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RootSystems2DComponent } from './components/root-systems2-d/root-systems2-d.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { TransformPanelComponent } from './components/transform-panel/transform-panel.component';
+import { SwitchRootSystemPanelComponent } from './components/switch-root-system-panel/switch-root-system-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootSystems2DComponent,
+    HomepageComponent,
+    TransformPanelComponent,
+    SwitchRootSystemPanelComponent
   ],
   imports: [
     BrowserModule,
